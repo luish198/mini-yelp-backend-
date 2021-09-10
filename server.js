6ff66de60398e305c913d087897ff19234e6f579
luish198 <luish198@hotmail.com>
2021-09-10 22:42:58 +0200

    7th attempt to fix port bug

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 import express from 'express';
-//import mysql from "mysql";
 import dotenv from "dotenv"
 import cors from 'cors';
 
 import restaurantRouter from "./routes/restaurants.js";
 import cuisineRouter from "./routes/cuisine.js";
 
-
-//const homeRouter =require('./routes/home')
-
-dotenv.config()
-
-const port = 3000
-//const PORT = process.env.PORT || 5000;
-
 const app = express()
-
 app.use(express.json())
 app.use(cors());
+
+dotenv.config()
 
 
 app.use(express.urlencoded({extended: true}))
@@ -25,11 +17,6 @@ app.use(express.json())
 
 app.use('/restaurants', restaurantRouter);
 app.use('/cuisine', cuisineRouter);
-
-
-/*app.listen(process.env.PORT || 3000,()=>{
-    console.log("server running on port"+port)
-})*/
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is running on 3000 or process.env.PORT ")
